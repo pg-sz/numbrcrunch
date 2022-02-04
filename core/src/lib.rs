c02eca5149c7c022ec5ce1d555c77c2e29ca134f
Peter Gruszka <gruszka.peter@mailbox.org>
2022-02-04 19:18:59 +0100

    NUM-3 added Variable Repository; adjusted tests; added new test

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,4 +1,6 @@
+use std::collections::{HashMap};
 
+type VariableRepository<'a> = HashMap<&'a str, f64>;
 
 enum Expression {
     Value(f64),
@@ -11,18 +13,18 @@ enum Expression {
 }
 
 trait Eval {
-    fn eval(&self) -> f64;
+    fn eval(&self, variable_repository: &VariableRepository) -> f64;
 }
 
 impl Eval for Expression {
-    fn eval(&self) -> f64 {
+    fn eval(&self, variable_repository: &VariableRepository) -> f64 {
         match self {
             Expression::Value(x) => *x,
-            Expression::Variable(_) => 0.0, // TODO:
-            Expression::Add(lhs, rhs) => lhs.eval() + rhs.eval(),
-            Expression::Subtract(lhs, rhs) => lhs.eval() - rhs.eval(),
-            Expression::Multiply(lhs, rhs) => lhs.eval() * rhs.eval(),
-            Expression::Divide(numerator, denominator) => numerator.eval() / denominator.eval(),
+            Expression::Variable(name) => *variable_repository.get(name.as_str()).expect("variable unknown."),
+            Expression::Add(lhs, rhs) => lhs.eval(variable_repository) + rhs.eval(variable_repository),
+            Expression::Subtract(lhs, rhs) => lhs.eval(variable_repository) - rhs.eval(variable_repository),
+            Expression::Multiply(lhs, rhs) => lhs.eval(variable_repository) * rhs.eval(variable_repository),
+            Expression::Divide(numerator, denominator) => numerator.eval(variable_repository) / denominator.eval(variable_repository),
             Expression::Function(_, _) => 0.0, // TODO: plan function mapper
         }
     }
@@ -30,52 +32,68 @@ impl Eval for Expression {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Eval, Expression};
+    use std::collections::HashMap;
+    use crate::{Eval, Expression, VariableRepository};
 
     #[test]
     fn it_value() {
+        let repository: VariableRepository = VariableRepository::new();
         let mock_value = 1.0;
         let value = Expression::Value(mock_value);
-        assert_eq!(mock_value, value.eval());
+        assert_eq!(mock_value, value.eval(&repository));
     }
 
     #[test]
     fn it_addition() {
+        let repository: VariableRepository = VariableRepository::new();
         let mock_values = [1.0, 2.0];
         let expected_result = 3.0;
         let value1 = Expression::Value(*mock_values.get(0).expect("not found"));
         let value2 = Expression::Value(*mock_values.get(1).expect("not found"));
-        let addition = Expression::Add(Box::new(value1), Box::new(value2));
-        assert_eq!(expected_result, addition.eval());
+        let result = Expression::Add(Box::new(value1), Box::new(value2));
+        assert_eq!(expected_result, result.eval(&repository));
     }
 
     #[test]
     fn it_subtract() {
+        let repository: VariableRepository = VariableRepository::new();
         let mock_values = [1.0, 2.0];
         let expected_result = -1.0;
         let value1 = Expression::Value(*mock_values.get(0).expect("not found"));
         let value2 = Expression::Value(*mock_values.get(1).expect("not found"));
-        let addition = Expression::Subtract(Box::new(value1), Box::new(value2));
-        assert_eq!(expected_result, addition.eval());
+        let result = Expression::Subtract(Box::new(value1), Box::new(value2));
+        assert_eq!(expected_result, result.eval(&repository));
     }
 
     #[test]
     fn it_multiply() {
+        let repository: VariableRepository = VariableRepository::new();
         let mock_values = [2.0, 3.0];
         let expected_result = 6.0;
         let value1 = Expression::Value(*mock_values.get(0).expect("not found"));
         let value2 = Expression::Value(*mock_values.get(1).expect("not found"));
-        let addition = Expression::Multiply(Box::new(value1), Box::new(value2));
-        assert_eq!(expected_result, addition.eval());
+        let result = Expression::Multiply(Box::new(value1), Box::new(value2));
+        assert_eq!(expected_result, result.eval(&repository));
     }
 
     #[test]
     fn it_divide() {
+        let repository: VariableRepository = VariableRepository::new();
         let mock_values = [3.0, 2.0];
         let expected_result = 1.5;
         let value1 = Expression::Value(*mock_values.get(0).expect("not found"));
         let value2 = Expression::Value(*mock_values.get(1).expect("not found"));
-        let addition = Expression::Divide(Box::new(value1), Box::new(value2));
-        assert_eq!(expected_result, addition.eval());
+        let result = Expression::Divide(Box::new(value1), Box::new(value2));
+        assert_eq!(expected_result, result.eval(&repository));
+    }
+
+    #[test]
+    fn it_test_repository() {
+        let mut repository: VariableRepository = VariableRepository::new();
+        repository.insert("x", 1.0);
+
+        let result = Expression::Variable("x".to_string());
+
+        assert_eq!(1.0, result.eval(&repository));
     }
 }
