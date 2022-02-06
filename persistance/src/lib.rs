77b3a232d3119b4b6185e1490f4ca36e2e4c9d4a
Peter Gruszka <gruszka.peter@mailbox.org>
2022-02-06 16:14:09 +0100

    NUM-6: added first test with serde

--- persistance/src/lib.rs
+++ persistance/src/lib.rs
@@ -1,11 +1,24 @@
 
-
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+enum Test<'a> {
+    Ass(&'a str),
+    Jake(i32, f64),
+    Queen(Box<Test<'a>>, Box<Test<'a>>),
+    King(bool),
+}
 
 #[cfg(test)]
 mod tests {
+    use crate::Test;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let test = Test::Queen(
+            Box::new(Test::Jake(1, 2.0)),
+            Box::new(Test::King(false))
+        );
+        let s = serde_yaml::to_string(&test).expect("panic");
+        assert_eq!(s, "---\nQueen:\n  - Jake:\n      - 1\n      - 2.0\n  - King: false\n");
     }
+
 }
